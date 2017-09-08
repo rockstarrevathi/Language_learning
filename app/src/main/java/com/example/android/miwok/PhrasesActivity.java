6c5d1585e95f75c2bc520bf242daf8ddf4845bc5
rockstarrevathi <jannavarapu.revathi@gmail.com>
2017-09-08 09:22:05 -0500

    added Audio Focus

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,11 @@ import java.util.ArrayList;
 public class PhrasesActivity extends AppCompatActivity {
     //Handles playbacks of all the sound files
     private MediaPlayer mMediaPlayer;
+
+    /**
+     * Handles audio focus when playing a sound file
+     */
+    private AudioManager mAudioManager;
     /**
      * This listener gets triggered when the {@link MediaPlayer} has completed
      * playing the audio file.
@@ -23,27 +30,60 @@ public class PhrasesActivity extends AppCompatActivity {
             releaseMediaPlayer();
         }
     };
+
+    /**
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                // both cases the same way because our app is playing short sound files.
+
+                // Pause playback and reset player to the start of the file. That way, we can
+                // play the word from the beginning when we resume playback.
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
+        // Create and setup the {@link AudioManager} to request audio focus
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
         // Create a list of words
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("Where are you going?", "Ekkadiki Velthunnav?",R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?", "Ne peru anti?",R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...", "Na peru ....",R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?", "Ela unnav?",R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I’m feeling good.", "Nenu Baunna",R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?", "Nuvvu Vasthunnava?",R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I’m coming.", "Ha, nenu Vasthunna",R.raw.phrase_yes_im_coming));
-        words.add(new Word("I’m coming.", "Vasthunna",R.raw.phrase_im_coming,R.raw.phrase_im_coming));
-        words.add(new Word("Let’s go.", "Padha veldam",R.raw.phrase_lets_go));
-        words.add(new Word("Come here.", "Ekkadiki ra",R.raw.phrase_come_here));
+        words.add(new Word("Where are you going?", "Ekkadiki Velthunnav?", R.raw.phrase_where_are_you_going));
+        words.add(new Word("What is your name?", "Ne peru anti?", R.raw.phrase_what_is_your_name));
+        words.add(new Word("My name is...", "Na peru ....", R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?", "Ela unnav?", R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("I’m feeling good.", "Nenu Baunna", R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?", "Nuvvu Vasthunnava?", R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes, I’m coming.", "Ha, nenu Vasthunna", R.raw.phrase_yes_im_coming));
+        words.add(new Word("I’m coming.", "Vasthunna", R.raw.phrase_im_coming, R.raw.phrase_im_coming));
+        words.add(new Word("Let’s go.", "Padha veldam", R.raw.phrase_lets_go));
+        words.add(new Word("Come here.", "Ekkadiki ra", R.raw.phrase_come_here));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
-        WordAdapter adapter = new WordAdapter(this, words,R.color.category_phrases);
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_phrases);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
@@ -67,18 +107,39 @@ public class PhrasesActivity extends AppCompatActivity {
                 //create and setup the {@link MediaPlayer for the audio resource associated
                 //with the current word
                 mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getmAudioResourceId());
-                //start the audio file
-                mMediaPlayer.start();
-                // Setup a listener on the media player, so that we can stop and release the
-                // media player once the sound has finished playing.
-                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+
+                // Request audio focus so in order to play the audio file. The app needs to play a
+                // short audio file, so we will request audio focus with a short amount of time
+                // with AUDIOFOCUS_GAIN_TRANSIENT.
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // We have audio focus now.
+
+                    //start the audio file
+                    mMediaPlayer.start();
+
+                    // Setup a listener on the media player, so that we can stop and release the
+                    // media player once the sound has finished playing.
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
 
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
     /**
      * Clean up the media player by releasing its resources.
      */
+
     private void releaseMediaPlayer() {
         // If the media player is not null, then it may be currently playing a sound.
         if (mMediaPlayer != null) {
@@ -90,6 +151,10 @@ public class PhrasesActivity extends AppCompatActivity {
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
             mMediaPlayer = null;
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
         }
     }
 }
